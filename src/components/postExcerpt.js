fcff1018d55b0cba51c3aad29134ccd573d615dc
Dustin Knopoff <dustinknopoff@gmail.com>
2019-03-23 23:06:10 -0400

    refactor(all): backticks

--- src/components/postExcerpt.js
+++ src/components/postExcerpt.js
@@ -4,20 +4,20 @@ import Moment from "react-moment"
 import { Link } from "gatsby"
 
 const PostExcerpt = ({ node }) => (
-  <Link to={node.fields.slug}>
-    <TitleDate>
-      <h4 style={{ textAlign: `right` }}>{node.frontmatter.title}</h4>
-      <Moment fromNow>{node.fields.modifiedTime}</Moment>
-    </TitleDate>
-    <p style={{ marginLeft: `25%` }}>{node.excerpt}</p>
-  </Link>
+    <Link to={node.fields.slug}>
+        <TitleDate>
+            <h4 style={{ textAlign: `right` }}>{node.frontmatter.title}</h4>
+            <Moment fromNow>{node.fields.modifiedTime}</Moment>
+        </TitleDate>
+        <p style={{ marginLeft: `25%` }}>{node.excerpt}</p>
+    </Link>
 )
 
 export default PostExcerpt
 
 export const TitleDate = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row-reverse;
-  align-items: baseline;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row-reverse;
+    align-items: baseline;
 `
