579ac95d4e9f2dc1097f7bd584823d56c6f20754
Dustin Knopoff <dustinknopoff@gmail.com>
2019-05-28 07:24:07 -0400

    feat(code highlighting): uses dark theme when prefers color scheme dark

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,5 +5,9 @@
  */
 
 // You can delete this file if you're not using it
-require("prismjs/themes/prism-solarizedlight.css")
+if (window.matchMedia("(prefers-color-scheme: dark)")) {
+    require("prismjs/themes/prism-tomorrow.css")
+} else {
+    require("prismjs/themes/prism-solarizedlight.css")
+}
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
